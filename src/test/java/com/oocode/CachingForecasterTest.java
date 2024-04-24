18a363f858de546d18eebef4626558813adab8f4
ape06 <awjoha@gmail.com>
2024-04-24 15:36:47 +0100

    caching test fails

--- src/test/java/com/oocode/CachingForecasterTest.java
+++ src/test/java/com/oocode/CachingForecasterTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import java.time.DayOfWeek;
 
 import static java.time.DayOfWeek.FRIDAY;
+import static java.time.DayOfWeek.MONDAY;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.BDDMockito.given;
@@ -28,5 +29,26 @@ public class CachingForecasterTest {
 
         verify(delegate).forecastFor("Oxford", FRIDAY); // This is arguable
         assertThat(forecast, equalTo(expectedForecast));
+    }
+
+    @Test
+    public void delegatesForecastingCachingRepeatedForecast() {
+        var delegate = mock(Forecaster.class);
+        Forecast expectedForecast = new Forecast(1, 2, FRIDAY);
+        given(delegate.forecastFor("Oxford", FRIDAY)).willReturn(expectedForecast);
+
+        var underTest = new CachingForecaster(delegate);
+
+        var forecast = underTest.forecastFor("Oxford", FRIDAY);
+        assertThat(forecast, equalTo(expectedForecast));
+
+        Forecast expectedForecast2 = new Forecast(2, 3, FRIDAY);
+        given(delegate.forecastFor("Oxford", FRIDAY)).willReturn(expectedForecast2);
+
+
+        var forecast2 = underTest.forecastFor("Oxford", FRIDAY);
+
+        assertThat(forecast2, equalTo(expectedForecast));
+
     }
 }
